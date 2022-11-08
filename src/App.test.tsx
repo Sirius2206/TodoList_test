eff82e9e2e6ed89edb119f19dfc6aa69a56508b0
unknown <samurai6022@gmail.com>
2022-11-08 21:29:41 +0300

    Initial

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,46 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, cleanup } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe("App tests", () => {
+  beforeEach(() => {
+    render(<App />);
+  })
+  
+  afterEach(() => {
+    cleanup();
+  })
+
+  it("should display all 4 buttons", () => {
+    expect(screen.getByTestId("all")).toBeInTheDocument();
+    expect(screen.getByTestId("active")).toBeInTheDocument();
+    expect(screen.getByTestId("completed")).toBeInTheDocument();
+    expect(screen.getByTestId("clear")).toBeInTheDocument();
+  }),
+
+  it("should create todo element", () => {
+    const input = document.querySelector(".input")! as HTMLInputElement;
+    input.value = "test";
+    const form = document.querySelector("form")! as HTMLFormElement;
+    act(() => {
+      form.submit();
+    })
+    expect(screen.getByText("test")).toBeInTheDocument();
+  }),
+
+  it("should complete todo task", () => {
+    const input = document.querySelector(".input")! as HTMLInputElement;
+    input.value = "test";
+    const form = document.querySelector("form")! as HTMLFormElement;
+    act(() => {
+      form.submit();
+    })
+    act(() => {
+      const todoElem = document.querySelector(".todo-elem")!;
+      if (todoElem instanceof HTMLElement) {
+        todoElem.click();
+      }
+    })
+    expect(document.querySelector(".todo-completed")!).toBeInTheDocument();
+  })
+  })
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,114 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { FormEvent, useRef, useState } from 'react'
+import './App.css'
+import { nanoid } from 'nanoid'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type TodoProps = {
+  todo: string;
+  completed: boolean;
 }
 
-export default App;
+
+
+function App() {
+  const [todos, setTodos] = useState<TodoProps[]>([]);
+  const [mod, setMod] = useState<string>("all")
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  function addTodo(e: FormEvent) {
+    const value = inputRef.current!.value;
+    e.preventDefault();
+    if (value) {
+      setTodos(prevState => [...prevState, { todo: value, completed: false }]);
+    }
+    inputRef.current!.value = "";
+  }
+
+  function toggleElem(elem: TodoProps) {
+    const index = todos.indexOf(elem);
+    let newTodos: TodoProps[] = Object.assign([], todos);
+    newTodos[index].completed = !newTodos[index].completed;
+    setTodos(newTodos);
+  }
+
+  function filterTodos(todo: TodoProps) {
+    if (mod !== "true" && mod !== "false") return todo;
+    return mod === "true" ? todo.completed === true : todo.completed === false;
+  }
+
+  function clearCompleted() {
+    setTodos(todos.filter(todo => todo.completed !== true))
+  }
+
+  function changeMod(e: React.MouseEvent<HTMLButtonElement, MouseEvent>, newMod: string) {
+    const btn: HTMLButtonElement = e.target! as HTMLButtonElement;
+    if (btn.classList.contains("btn-active")) return;
+    setMod(newMod);
+    document.querySelector(".btn-active")?.classList.remove("btn-active");
+    btn.classList.add("btn-active");
+
+  }
+
+  return <>
+    <h1 className='main-title'>todos</h1>
+    <div className='container'>
+      <form onSubmit={e => addTodo(e)}>
+        <input className='input' ref={inputRef} placeholder='What needs to be done?'></input>
+      </form>
+      <ul>
+        {todos.filter(filterTodos).map((elem) =>
+          <li
+            key={nanoid()}
+            className={`todo-elem ` + (elem.completed ? "todo-completed" : "")}
+            onClick={() => toggleElem(elem)}
+          >
+            {elem.todo}
+          </li>
+        )}
+      </ul>
+      <footer className='footer'>
+        {todos.filter(item => item.completed === false).length > 0
+          ? <p>{todos.filter(item => item.completed === false).length} items left</p>
+          : <p>Completed</p>}
+        <ul className='todo-choose'>
+          <li>
+            <button
+              className="btn btn-active"
+              data-testid="all"
+              onClick={e => changeMod(e, "all")}
+            >
+              All
+            </button>
+          </li>
+          <li>
+            <button
+              className="btn"
+              data-testid="active"
+              onClick={e => changeMod(e, "false")}
+            >
+              Active
+            </button>
+          </li>
+          <li>
+            <button
+              className="btn"
+              data-testid="completed"
+              onClick={e => changeMod(e, "true")}
+            >
+              Completed
+            </button>
+          </li>
+        </ul>
+        <button 
+          className='todo-clear' 
+          data-testid="clear"
+          onClick={clearCompleted}
+          >
+            Clear completed
+          </button>
+      </footer>
+    </div>
+  </>
+}
+
+export default App
+
